1251272219381e1d34222c630bf15033150ecd86
Dhruvil Gajjar <dgajjar999@gmail.com>
2020-07-20 20:06:38 +0530

    add router dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
-  return <div className='app'></div>;
+  return (
+    <div className='app'>
+      <h1>Hello World</h1>
+    </div>
+  );
 }
 
 export default App;
