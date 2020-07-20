1edcfb1937b5d7ca0b0c62787103b5b8e81d31e5
Dhruvil Gajjar <dgajjar999@gmail.com>
2020-07-20 20:29:46 +0530

    add amazon logo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "./components/Header";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
             <h1>Login</h1>
           </Route>
           <Route path='/'>
+            <Header />
             <h1>Home Page</h1>
           </Route>
         </Switch>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 function Header() {
-  return <div></div>;
+  return (
+    <nav className='header'>
+      <img
+        src='http://pngimg.com/uploads/amazon/amazon_PNG11.png'
+        alt='amazon_logo'
+      />
+    </nav>
+  );
 }
 
 export default Header;
