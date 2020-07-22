853f22c90a05817b051542cd3eaa20086c92d4c4
Dhruvil Gajjar <dgajjar999@gmail.com>
2020-07-22 19:20:04 +0530

    add search bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import "../style/header.css";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <nav className='header'>
-      <img
-        className='header__logo'
-        src='http://pngimg.com/uploads/amazon/amazon_PNG11.png'
-        alt='amazon_logo'
-      />
+      <Link to='/'>
+        <img
+          className='header__logo'
+          src='http://pngimg.com/uploads/amazon/amazon_PNG11.png'
+          alt='amazon_logo'
+        />
+      </Link>
+
+      <input type='text' className='header__searchInput' />
     </nav>
   );
 }
