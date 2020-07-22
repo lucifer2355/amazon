9963823483fdf6eba7f917a8f38fb5a34f4655e3
Dhruvil Gajjar <dgajjar999@gmail.com>
2020-07-22 19:15:54 +0530

    add header css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,7 @@ function Header() {
   return (
     <nav className='header'>
       <img
+        className='header__logo'
         src='http://pngimg.com/uploads/amazon/amazon_PNG11.png'
         alt='amazon_logo'
       />
