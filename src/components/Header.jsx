b422d44a3d09037dab25408dde6e02f1d68eba13
Dhruvil Gajjar <dgajjar999@gmail.com>
2020-07-22 20:02:05 +0530

    add header sticky

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "../style/header.css";
 import { Link } from "react-router-dom";
 import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 
 function Header() {
   return (
@@ -22,22 +23,29 @@ function Header() {
       <div className='header__nav'>
         <Link to='/login' className='header__link'>
           <div className='header__option'>
-            <span>Hello</span>
-            <span>Sign In</span>
+            <span className='header__optionLineOne'>Hello</span>
+            <span className='header__optionLineTwo'>Sign In</span>
           </div>
         </Link>
 
         <Link to='/login' className='header__link'>
           <div className='header__option'>
-            <span>Return</span>
-            <span>& Orders</span>
+            <span className='header__optionLineOne'>Return</span>
+            <span className='header__optionLineTwo'>& Orders</span>
           </div>
         </Link>
 
         <Link to='/' className='header__link'>
           <div className='header__option'>
-            <span>Your</span>
-            <span>Prime</span>
+            <span className='header__optionLineOne'>Your</span>
+            <span className='header__optionLineTwo'>Prime</span>
+          </div>
+        </Link>
+
+        <Link to='/checkout' className='header__link'>
+          <div className='header__optionBasket'>
+            <ShoppingBasketIcon />
+            <span className='header__optionLineTwo header__basketCount'>0</span>
           </div>
         </Link>
       </div>
