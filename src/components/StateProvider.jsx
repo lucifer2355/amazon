e1623558ac12f85c744c893f3218a4e916fce0a7
Dhruvil Gajjar <dgajjar999@gmail.com>
2020-07-23 22:20:44 +0530

    add redux

--- src/components/StateProvider.jsx
+++ src/components/StateProvider.jsx
@@ -1,10 +1,8 @@
-import { createContext } from "react";
-
 import React, { createContext, useContext, useReducer } from "react";
 
 export const StateContext = createContext();
 
-export const StateProvider = (reducer, initialState, children) => {
+export const StateProvider = ({ reducer, initialState, children }) => {
   <StateContext.Provider value={useReducer(reducer, initialState)}>
     {children}
   </StateContext.Provider>;
